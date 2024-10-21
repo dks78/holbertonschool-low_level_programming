f394758639011745ffe9d710e1e99e0487a07559
root <root@Aurlien>
2024-10-21 14:17:25 +0200

    chal8

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,6 +1,12 @@
 #include <stdio.h>
 
-
+/**
+ * main - Entry point of the program
+ *
+ * Description: This function prints a specific string.
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
