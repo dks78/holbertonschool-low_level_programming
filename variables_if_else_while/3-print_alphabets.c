a4c77ab9ee388d3430b61a6d99e7a797a5649b37
aurelien <aurelienmest@outlook.fr>
2024-10-22 16:20:28 +0200

    chal8

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,25 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
- /**
-  * main - print alphabet in reverse lowerse 
-  *
-  * Return: returns 0
-  *
- */
+/**
+ * main - print alphabet in reverse lowerse 
+ * Return: returns 0
+ *
+*/
 int main(void)
 {
+	char c;
 
-    char c;
+	for (c = 'a'; c <= 'z'; c++)
+	{
+		putchar(c);
+	}
+	for (c = 'A'; c <= 'Z'; c++)
+	{
+		putchar(c);
+	}
+		putchar('\n');
 
-    for (c = 'a'; c <= 'z'; c++)
-    {
-        putchar(c);
-    }
-        for (c = 'A'; c <= 'Z'; c++)
-    {
-        putchar(c);
-    }
-    putchar('\n');
-    return 0;
+	return (0);
 }
