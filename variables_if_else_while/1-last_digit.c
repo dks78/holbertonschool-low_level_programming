4b60c58a22600128a294a292c98445a740b2d38a
aurelien <aurelienmest@outlook.fr>
2024-10-22 11:50:19 +0200

    chal8

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -3,18 +3,18 @@
 #include <time.h>
 
 int main() {
-    int n;             // Déclaration de n
-    int last_digit;    // Déclaration de last_digit
+    int n;          
+    int last_digit;    
 
     srand(time(0));
     n = rand() - RAND_MAX / 2;
 
-    last_digit = n % 10; // Obtenir le dernier chiffre
+    last_digit = n % 10; 
 
-    // Afficher le résultat
+
     printf("Last digit of %d is %d ", n, last_digit);
 
-    // Conditions pour afficher le message approprié
+   
     if (last_digit > 5) {
         printf("and is greater than 5\n");
     } else if (last_digit == 0) {
