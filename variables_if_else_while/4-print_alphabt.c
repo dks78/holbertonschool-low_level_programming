cd5bda61a3ebe1775ad9ff681b4b66f4a349c22e
aurelien <aurelienmest@outlook.fr>
2024-10-22 17:20:41 +0200

    chal8

---
+++ variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+ /**
+  * main - print alphabet in reverse lowoercase
+  *
+  *
+  * Return: returns 0
+  */
+int main(void)
+{
+
+    char c;
+
+    for (c = 'a'; c <= 'z'; c++) 
+    {
+        if( c != 'e' && c != 'q' ){
+            putchar(c);
+        }
+    }
+    putchar('\n');
+    return (0);
+}
