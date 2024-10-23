8da8c460823a4b68ff37bc2e980e72a517f9b203
aurelien <aurelienmest@outlook.fr>
2024-10-23 16:01:58 +0200

    chal8

--- functions_nested_loops/test/main.c
+++ functions_nested_loops/test/main.c
@@ -2,21 +2,28 @@
 #include <stdlib.h>
 #include <time.h>
 
+int _islower(int c); // prototype islower
 
-
+// Fonction principale pour tester _islower
 int main(void) 
-{ 
-    int i;
-    char lettre;
-
-    for (i = 0 ; i < 10 ; i++)
-    {
-         for(lettre = 'a'; lettre <= 'z'; lettre++)
-         {
-            putchar(lettre);
-         }
-          putchar('\n');
-    }
-   
+{
+    char c = 'g'; 
     return (0);
+}
+
+// Implémentation de la fonction _islower
+int _islower(int c)
+{
+    // Vérifie si c est dans la plage des lettres minuscules
+    if (c >= 'a' && c <= 'z')
+    {
+        return 1; // C'est une lettre minuscule
+    }
+    else
+    {
+        return 0; // Ce n'est pas une lettre minuscule
+    }
+
+    putchar(c);
+
 }
