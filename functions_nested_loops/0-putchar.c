2e0e987874f2bf1ebaf607af242494410bf4a0ca
aurelien <aurelienmest@outlook.fr>
2024-10-23 11:20:10 +0200

    chal8

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+/**
+ * main - print to variable
+ *
+ *
+ * Return: 0 Always (Success)
+ *
+ *
+*/
+int main(void)
+{
+    printf("_putchar\n");
+    return 0;
+}
