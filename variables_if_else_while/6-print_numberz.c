947f177088f7966b7e45490c65664ccbc4a07475
aurelien <aurelienmest@outlook.fr>
2024-10-23 02:39:14 +0200

    chal8

--- variables_if_else_while/6-print_numberz.c
+++ variables_if_else_while/6-print_numberz.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <time.h>
 /**
- * main - print to variable 
+ * main - print to variable
  *
  *
  * Return: 0 Always (Success)
@@ -12,12 +12,14 @@
 int main(void)
 {
 	int i;
-	for( i = 0 ; i < 10 ; i++)
+
+	for (i = 0 ; i < 10 ; i++)
+
 	{
 		putchar(i + '0');
 
 	}
 	putchar('\n');
-	
+
 	return (0);
 }
