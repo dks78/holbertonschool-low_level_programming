8e932b1103724601e8562b282b4cabb5005e6948
aurelien <aurelienmest@outlook.fr>
2024-10-23 14:49:53 +0200

    chal8

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,10 +1,9 @@
 #include "main.h"
-
 /**
  * main - Entry point of the program
  *
  * Return: Always 0 (Success)
- */
+*/
 int main(void)
 {
 	_putchar('_');
--- functions_nested_loops/1-alphabet.c
+++ functions_nested_loops/1-alphabet.c
@@ -1,11 +1,15 @@
 #include "main.h"
-
+/**
+ * File: 1-alphabet.c
+ *
+ * Auth: Brennan D Baraban
+*/
 void print_alphabet(void)
 {
-    char letter;
-    for (letter = 'a'; letter <= 'z'; letter++)
-    {
-        _putchar(letter);
-    }
-    _putchar('\n');
+char letter;
+for (letter = 'a'; letter <= 'z'; letter++)
+{
+_putchar(letter);
+}
+_putchar('\n');
 }
