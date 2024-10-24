905216f10f9cc032197a5ffd1776a9d8aa83750f
aurelien <aurelienmest@outlook.fr>
2024-10-24 11:00:56 +0200

    chal8

--- functions_nested_loops/5-sign.c
+++ functions_nested_loops/5-sign.c
@@ -1,23 +1,24 @@
 #include "main.h"
 /**
- *  Return: 1 if c is lowercase, 0 otherwise
- * _islower - checks if a character is lowercase
- *  @c: The character to be checked
+ * print_sign - check sign.
+ * @n: integer.
+ * Return: Always 0.
 */
 int print_sign(int n)
 {
-    if(n > 0)
-    {
-        _putchar('+');
-        return(1);
-    } else if(n == 0)
-    {
-        _putchar('0');
-        return (0);
-    } else if( n < 0)
-    {
-        _putchar('-');
-        return(-1);
-    }
-    return 0;
+	if (n > 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	else if (n == 0)
+	{
+		_putchar('0');
+		return (0);
+	}
+	else
+	{
+		_putchar('-');
+		return (-1);
+	}
 }
