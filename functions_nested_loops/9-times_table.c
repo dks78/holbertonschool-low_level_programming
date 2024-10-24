3410e0376def521fc19cec36ad1088bf859eacfe
aurelien <aurelienmest@outlook.fr>
2024-10-24 17:46:11 +0200

    chal8

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,31 +1,32 @@
-#include "main.h"
+"main.h"
 #include <stdlib.h>
 /**
-  * jack_bauer - Prints every minute of the day of Jack Bauer
+  * times_table - Prints the 9 times table, starting with 0.
   *
   * Return: ...
   */
-void times_table(void) {
-    int i, j, product;
+void times_table(void)
+{
+	int num, mult, prod;
 
-    for (i = 0; i <= 9; i++) {
-        for (j = 0; j <= 9; j++) {
-            product = i * j;
+	for (num = 0; num <= 9; num++)
+	{
+		_putchar('0');
 
-            if (j == 0) {
-                _putchar(product + '0');
-            } else {
-                _putchar(',');
-                _putchar(' ');
-                if (product < 10) {
-                    _putchar(' '); 
-                    _putchar(product + '0'); 
-                } else {
-                    _putchar((product / 10) + '0');
-                    _putchar((product % 10) + '0'); 
-                }
-            }
-        }
-        _putchar('\n'); 
-    }
+		for (mult = 1; mult <= 9; mult++)
+		{
+			_putchar(',');
+			_putchar(' ');
+
+			prod = num * mult;
+
+			if (prod <= 9)
+				_putchar(' ');
+			else
+				_putchar((prod / 10) + '0');
+
+			_putchar((prod % 10) + '0');
+		}
+		_putchar('\n');
+	}
 }
