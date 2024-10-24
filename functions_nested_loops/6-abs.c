e9ab819ccd06008c3f3790f23e9d0e1d3e7d79d3
aurelien <aurelienmest@outlook.fr>
2024-10-24 11:56:53 +0200

    chal8

--- functions_nested_loops/6-abs.c
+++ functions_nested_loops/6-abs.c
@@ -5,10 +5,15 @@
  * @n: integer.
  * Return: Always 0.
 */
-int _abs(int n ) 
-{ 
-    if ( n < 0){
-        return -n;
-    }
-    return n;
+int _abs(int n)
+{
+
+if (n < 0)
+
+{
+return (-n);
+}
+
+return (n);
+
 }
