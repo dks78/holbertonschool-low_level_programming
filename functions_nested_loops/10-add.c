c1fbeeac9d730b2d8f0fde4313af58fa9f7cb5bd
aurelien <aurelienmest@outlook.fr>
2024-10-25 10:25:08 +0200

    chal8

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+/**
+  * times_table - Prints the 9 times table, starting with 0.
+  *
+  * Return: ...
+  */
+int add(int a, int b)
+{
+
+    return a + b;
+}
+int main(void)
+{
+    int n;
+
+    n = add(89, 9);
+    printf("%d\n", n);
+    return (0);
+}
--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 #include <stdlib.h>
 /**
   * times_table - Prints the 9 times table, starting with 0.
