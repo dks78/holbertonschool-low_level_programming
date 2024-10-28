edde95cd6cb1d3a3e783d83562740f25a6491c8c
aurelien <aurelienmest@outlook.fr>
2024-10-28 16:22:14 +0100

    chal8

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -2,18 +2,31 @@
 #include <stdio.h>
  /**
  *
- * more_numbers - Prints numbers from 0 to 9, excluding 2 and 4.
+ * print_line - Prints numbers from 0 to 9, excluding 2 and 4.
  * n: charactere
  * Return: Nothing (void)
  */
 
 void print_line(int n)
-{ 
-    int i;
-    if (n > 0) { 
-        for (i = 0; i < n; i++) {
-            _putchar('_');
-        }
-    }
-    _putchar('\n');
+{
+
+int i;
+
+if (n > 0)
+
+{
+
+for (i = 0; i < n; i++)
+
+{
+
+_putchar('_');
+
 }
+
+}
+
+_putchar('\n');
+
+}
+
