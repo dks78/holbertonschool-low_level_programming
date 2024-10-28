51a0dc3a4b8fa63f948050c37c4f437d1f29f0c0
aurelien <aurelienmest@outlook.fr>
2024-10-28 11:17:01 +0100

    chal8

--- more_functions_nested_loops/0-isupper.c
+++ more_functions_nested_loops/0-isupper.c
@@ -1,15 +1,20 @@
 #include "main.h"
+/**
+ * _isupper - function
+ *
+ * print lphabet_x10 - Prints the alphabet in lowercase followed by a newline
+ * @c: charactere
+ * Return: Nothing (void)
+*/
+int _isupper(int c)
+{
 
-int _isupper(int c) {
-    if (c >= 'A' && c <= 'Z')
+if (c >= 'A' && c <= 'Z')
 
-    return 1;
-    else
-    return 0;
+return (1);
+
+else
+
+return (0);
 
 }
-
-
-
-   
-
