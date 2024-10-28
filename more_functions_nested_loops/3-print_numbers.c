2431257df8032515d64df5bd50699df735f8bdf3
aurelien <aurelienmest@outlook.fr>
2024-10-28 12:21:56 +0100

    chal8

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * _isdigit - function
+ *
+ * print lphabet_x10 - Prints the alphabet in lowercase followed by a newline
+ * @c: charactere
+ * Return: Nothing (void)
+*/
+void print_numbers(void)
+{
+    char n;
+    for ( n = '0' ; n <= '9' ; n++)
+
+    putchar(n);
+    putchar('\n');
+    putchar('\n');
+}
