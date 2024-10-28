105b2f807ec46977f5dde549545e3e5fa84d52c7
aurelien <aurelienmest@outlook.fr>
2024-10-28 15:42:10 +0100

    chal8

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -2,20 +2,24 @@
 #include <stdio.h>
  /**
  *
- *  print_most_numbers - Prints numbers from 0 to 9, excluding 2 and 4.
- *  n: charactere
- *  Return: Nothing (void)
+ * more_numbers - Prints numbers from 0 to 9, excluding 2 and 4.
+ * n: charactere
+ * Return: Nothing (void)
  */
- void more_numbers(void) {
-    int i;
-    int n;
-    for (i = 0; i < 10; i++) {
-        for ( n = 0; n <= 14; n++) {
-            if (n > 9) {
-                _putchar('1');
-            }
-            _putchar((n % 10) + '0');
-        }
-        _putchar('\n');
-    }
+void more_numbers(void)
+
+{
+
+int i;
+int n;
+
+for (i = 0; i < 10; i++)
+
+for (n = 0; n <= 14; n++)
+
+if (n > 9)
+_putchar('1');
+_putchar((n % 10) + '0');
+_putchar('\n');
+
 }
