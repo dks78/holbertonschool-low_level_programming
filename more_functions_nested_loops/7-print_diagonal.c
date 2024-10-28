18b0b88d433db6b1b14c4c846418dcd660452de6
aurelien <aurelienmest@outlook.fr>
2024-10-28 16:54:15 +0100

    chal8

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -10,14 +10,14 @@ void print_diagonal(int n) {
     int i;
     int j;
     if (n <= 0) {
-        putchar('\n');
+        _putchar('\n');
     } else {
         for (i = 0; i < n; i++) {
             for (j = 0; j < i; j++) {
-                putchar(' ');
+                _putchar(' ');
             }
-            putchar('\\');
-            putchar('\n');
+            _putchar('\\');
+            _putchar('\n');
         }
     }
 }
