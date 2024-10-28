4cd664bf04d394c17cf72163d63cf00a26588127
aurelien <aurelienmest@outlook.fr>
2024-10-28 15:38:09 +0100

    chal8

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -6,17 +6,15 @@
  *  n: charactere
  *  Return: Nothing (void)
  */
-void more_numbers(void) {
+ void more_numbers(void) {
     int i;
     int n;
-    for(i = 0; i < 10; i++) {
-        for(n = 0; n <= 14; n++) { 
+    for (i = 0; i < 10; i++) {
+        for ( n = 0; n <= 14; n++) {
             if (n > 9) {
                 _putchar('1');
-                _putchar(n % 10 + '0'); 
-            } else {
-                _putchar(n + '0');
             }
+            _putchar((n % 10) + '0');
         }
         _putchar('\n');
     }
