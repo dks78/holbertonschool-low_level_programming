a497208f9e6574990bba20466a36f6a2f1558824
aurelien <aurelienmest@outlook.fr>
2024-10-28 15:29:19 +0100

    chal8

--- more_functions_nested_loops/4-print_most_numbers.c
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include "main.h"
  /**
  *
- *  print_most_numbers - function to print 0 to 9
+ *  print_most_numbers - Prints numbers from 0 to 9, excluding 2 and 4.
  *  n: charactere
  *  Return: Nothing (void)
  */
---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+ /**
+ *
+ *  print_most_numbers - Prints numbers from 0 to 9, excluding 2 and 4.
+ *  n: charactere
+ *  Return: Nothing (void)
+ */
+void more_numbers(void) {
+    int i;
+    int n;
+    for(i = 0; i < 10; i++) {
+        for(n = 0; n <= 14; n++) { 
+            if (n > 9) {
+                _putchar('1');
+                _putchar(n % 10 + '0'); 
+            } else {
+                _putchar(n + '0');
+            }
+        }
+        _putchar('\n');
+    }
+}
