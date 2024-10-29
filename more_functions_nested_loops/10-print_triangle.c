600d79bd11616514e828fe9b5dce0b1c592afeb4
aurelien <aurelienmest@outlook.fr>
2024-10-29 03:18:38 +0100

    chal8

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -1,24 +1,44 @@
 #include "main.h"
  /**
  *
- * print_diagonal : Prints numbers from 0 to 9, excluding 2 and 4.
+ * print_triangle : Prints numbers from 0 to 9, excluding 2 and 4.
  * n: charactere
  * Return: Nothing (void)
  */
-void print_triangle(int size){ 
-    int i,j,k;
- if (size <= 0) {
-        _putchar('\n');
-        return;
-    }
+void print_triangle(int size)
+{
+int i, j, k;
 
-    for (i = 1; i <= size; i++) {
-        for (j = 0; j < size - i; j++) {
-            _putchar(' ');
-        }
-        for (k = 0; k < i; k++) {
-            _putchar('#');
-        }
-        _putchar('\n');
-    }
- }
+if (size <= 0)
+{
+
+_putchar('\n');
+return;
+
+}
+
+for (i = 1; i <= size; i++)
+
+{
+
+for (j = 0; j < size - i; j++)
+
+{
+
+_putchar(' ');
+
+
+}
+
+for (k = 0; k < i; k++)
+
+{
+
+_putchar('#');
+
+}
+
+_putchar('\n');
+
+}
+}
