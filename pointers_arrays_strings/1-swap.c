28baa9cee617e9e23a092372f57becd9cc116d63
aurelien <aurelienmest@outlook.fr>
2024-10-29 12:28:49 +0100

    chal8

--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,15 +1,16 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
  * swap_int - check the code
- *
+ * @a : variable
+ * @b : variable
  * Return: Always 0.
  */
 void swap_int(int *a, int *b)
-{   
-    int temps;
-    temps = *a;
-    *a = *b;
-    *b = temps;
+{
+
+int temps;
+temps = *a;
+*a = *b;
+*b = temps;
 }
