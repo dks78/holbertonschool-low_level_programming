1546162624197eef0ee8b7b5f81bdeea10593b43
aurelien <aurelienmest@outlook.fr>
2024-10-29 10:18:26 +0100

    chal8

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,12 +1,13 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * main - check the code 
- *
+ * reset_to_98 - check the code
+ * @n : pointeur
  * Return: Always 0.
  */
 void reset_to_98(int *n)
-{      
-    
-   *n = 98;
+{
+
+*n = 98;
+
 }
