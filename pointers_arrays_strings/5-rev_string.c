08f5b003e7f4e701c551bbb4245c196d1ec5730d
aurelien <aurelienmest@outlook.fr>
2024-10-30 16:49:06 +0100

    chal8

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -10,9 +10,12 @@
 
  void rev_string(char *s)
  {
+
     int i;      
+    
     for (i = 9; i >= 0; i--) 
         putchar(s[i]);
     printf("\n");
-    
+
+    s[0] = '\0'; /* effacce la chaine , donc le dexiemme prinf est */
  }
