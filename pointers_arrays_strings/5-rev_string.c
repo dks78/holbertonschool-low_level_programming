9f63f1f3e721f625425530564dd155408d23f55b
aurelien <aurelienmest@outlook.fr>
2024-10-30 16:54:38 +0100

    chal8

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -8,21 +8,74 @@
  */
 void rev_string(char *s)
 {
+    int length = 0;
+    int a = 0;
+    int temporary;
 
+    while (s[a++])
+        length++;
+    for (a = length - 1; a >= length / 2; a--)
+    {
+        temporary = s[a];
+        s[a] = s[length - a - 1];
+        s[length - a - 1] = temporary;
+    }
+}
+/*
+Bien sûr ! Analysons le code que tu as fourni, qui semble être une fonction pour inverser une chaîne de caractères (une représentation typique d'un tableau de caractères en C).
+
+Analyse du Code
+Voici le code que tu as partagé :
+
+c
+Copier le code
 int length = 0;
 int a = 0;
 int temporary;
 
 while (s[a++])
-
-length++;
+    length++;
 
 for (a = length - 1; a >= length / 2; a--)
 {
-
-temporary = s[a];
-s[a] = s[length - a - 1];
-s[length - a - 1] = temporary;
+    temporary = s[a];
+    s[a] = s[length - a - 1];
+    s[length - a - 1] = temporary;
 }
+Explication Étape par Étape
+Variables Déclarées:
 
+int length = 0;: Initialise une variable pour stocker la longueur de la chaîne.
+int a = 0;: Initialise une variable d'index a qui sera utilisée pour parcourir la chaîne.
+int temporary;: Déclare une variable temporaire qui sera utilisée pour échanger des caractères dans la chaîne.
+Calculer la Longueur de la Chaîne:
+
+c
+Copier le code
+while (s[a++])
+    length++;
+while (s[a++]) : Cette boucle parcourt la chaîne s jusqu'à ce qu'elle rencontre le caractère nul (\0), qui indique la fin de la chaîne en C.
+s[a++] vérifie le caractère à la position a et, si c'est un caractère valide (non nul), il incrémente a après la vérification.
+length++ : Pour chaque caractère valide rencontré, la longueur est incrémentée de 1. À la fin de cette boucle, length contiendra le nombre total de caractères dans la chaîne, sans compter le caractère nul.
+Inverser la Chaîne:
+
+c
+Copier le code
+for (a = length - 1; a >= length / 2; a--)
+{
+    temporary = s[a];
+    s[a] = s[length - a - 1];
+    s[length - a - 1] = temporary;
 }
+for (a = length - 1; a >= length / 2; a--) : Cette boucle parcourt la chaîne à l'envers, en commençant par le dernier caractère (length - 1) et en continuant jusqu'à la moitié de la chaîne.
+On utilise length / 2 pour ne traiter que la première moitié de la chaîne, car les caractères seront échangés avec les caractères correspondants de l'autre moitié. Cela évite de réécrire les caractères déjà échangés.
+temporary = s[a]; : On stocke le caractère actuel (à la position a) dans temporary.
+s[a] = s[length - a - 1]; : On remplace le caractère à la position a par son équivalent à l'autre extrémité de la chaîne.
+s[length - a - 1] = temporary; : On met le caractère stocké dans temporary à la position correspondante dans la chaîne, effectuant ainsi l'échange.
+Résumé
+Ce code a pour but d'inverser une chaîne de caractères en utilisant deux boucles :
+
+La première boucle calcule la longueur de la chaîne.
+La deuxième boucle échange les caractères pour inverser la chaîne en place, en parcourant la chaîne de la fin vers le milieu.
+À la fin de ce code, la chaîne s est complètement inversée, et le caractère nul (\0) reste à la fin pour indiquer la fin de la chaîne.
+*/
