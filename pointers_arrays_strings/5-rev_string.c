bf7232bee93b00063b53c6e2b813cd78cf88ff1c
aurelien <aurelienmest@outlook.fr>
2024-10-30 16:50:44 +0100

    chal8

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -1,21 +1,22 @@
 #include "main.h"
 #include <stdio.h>
-
+#include "main.h"
 /**
- * @s: check the code
- * print_rev: fonction
- * coch
+ *rev_string - reverses a string
+ *@s: string
+ *
  */
-
-
- void rev_string(char *s)
- {
-
-    int i;      
-    
-    for (i = 9; i >= 0; i--) 
-        putchar(s[i]);
-    printf("\n");
-
-    s[0] = '\0'; /* effacce la chaine , donc le dexiemme prinf est */
- }
+void rev_string(char *s)
+{
+        int length = 0;
+        int a = 0;
+        int temporary;
+        while (s[a++])
+        length++;
+        for (a = length - 1; a >= length / 2; a--)
+{
+        temporary = s[a];
+        s[a] = s[length - a - 1];
+        s[length - a - 1] = temporary;
+}
+}
