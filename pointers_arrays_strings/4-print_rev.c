eafd94012eafaf4d35078254f23f2e6917931933
aurelien <aurelienmest@outlook.fr>
2024-10-30 11:08:36 +0100

    chal8

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -5,23 +5,21 @@
  * @str: check the code
  * _puts: fonction
  * Return: Always 0.
- */   
-
-
+ */
 void print_rev(char *s)
 {
-   
-    int  length = 0;
+int  length = 0;
 
-    while ( s[length] != '\0')
-    {
+while (s[length] != '\0')
 
-        length++;
-    } 
-  
-    for(length -= 1; length >= 0; length--)
-    {
-    _putchar(s[length]);
-    }
-     _putchar('\n');
-} 
+{
+length++;
+
+}
+for (length -= 1; length >= 0; length--)
+{
+_putchar(s[length]);
+}
+_putchar('\n');
+}
+
