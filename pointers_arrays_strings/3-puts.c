a2bfbc24efb1c40c31bbb7f2ea4a458060c31356
aurelien <aurelienmest@outlook.fr>
2024-10-30 09:45:12 +0100

    chal8

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,16 +1,21 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - check the code
- *
+ * *str - check the code
+ * _puts : fonction
  * Return: Always 0.
  */
 void _puts(char *str)
 {
-    while(*str != '\0')
-    {
-        _putchar(*str);
-        str++;
-    }
-    _putchar('\n');
+
+while (*str != '\0')
+{
+_putchar(*str);
+
+str++;
+
+}
+
+_putchar('\n');
+
 }
--- pointers_arrays_strings/main.c
+++ pointers_arrays_strings/main.c
@@ -1,15 +1,14 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * main - check the code
+ * str ;- check the code
  *
  * Return: Always 0.
  */
 int main(void)
 {
-    char *str;
-
-    str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
-    puts(str);
-    return (0);
+char *str;
+str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+puts(str);
+return (0);
 }
