01cdade72efe19e8a113b6e339a9a3c94a4d8499
aurelien <aurelienmest@outlook.fr>
2024-10-31 14:46:24 +0100

    chal8

--- pointers_arrays_strings/1-strncat.c
+++ pointers_arrays_strings/1-strncat.c
@@ -9,19 +9,22 @@
  */
 char *_strncat(char *dest, char *src, int n)
 {
-    char *ptr = dest;
+char *ptr = dest;
 
-    while (*ptr != '\0') {
-        ptr++;
-    }
-    while (n > 0 && *src != '\0') {
-        *ptr = *src;
-        ptr++;
-        src++;
-        n--;
-    }
+while (*ptr != '\0')
+{
+ptr++;
+}
+while (n > 0 && *src != '\0')
+{
+*ptr = *src;
+ptr++;
+src++;
+n--;
+}
 
-    *ptr = '\0';
+*ptr = '\0';
 
-    return dest;
+return (dest);
+
 }
