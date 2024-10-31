6126bbae72fe5f522da466a9a17d3dc0f8d569e9
aurelien <aurelienmest@outlook.fr>
2024-10-31 12:34:53 +0100

    chal8

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+    
+ char *ptr = dest;
+while (*ptr != '\0') {
+    ptr++;
+}
+ while (*src != '\0') {
+        *ptr = *src;
+        ptr++;      
+        src++;       
+    }
+ *ptr = '\0';
+ return dest;
+}
