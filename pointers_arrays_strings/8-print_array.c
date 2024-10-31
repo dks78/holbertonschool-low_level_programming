140ecf6cbfd2d1f450fe0645a55a3747892d5bd1
aurelien <aurelienmest@outlook.fr>
2024-10-31 10:11:49 +0100

    chal8

--- pointers_arrays_strings/8-print_array.c
+++ pointers_arrays_strings/8-print_array.c
@@ -2,18 +2,26 @@
 #include <stdio.h>
 /**
  * main - check the code for
- *
+ * @a - variable
+ * @b - variable
  * Return: Always 0.
+ * print_array : fonction
  */
 void print_array(int *a, int n)
 {
-    int i;
-    for( i = 0; i < n ; i++)
-    {
-        printf("%d", a[i]);
-        if (i < n -1){
-            printf(", ");
-        }
-    }
-    printf("\n");
+int i;
+
+for (i = 0; i < n ; i++)
+
+{
+printf("%d", a[i]);
+
+if (i < n - 1)
+
+{
+
+printf(", ");
+}
+}
+printf("\n");
 }
