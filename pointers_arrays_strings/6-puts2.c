8d5286a494f0a1c7b93cdeb3f9467a439c24b266
aurelien <aurelienmest@outlook.fr>
2024-10-31 09:35:32 +0100

    chal8

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -8,11 +8,19 @@
 void puts2(char *str) {
     int len = 0;
     int i;
+    int start;
+    
     while (str[len] != '\0') {
         len++;
     }
 
-    for (i = 0; str[i] != '\0'; i+=2) {
+    if (len % 2 == 0) {
+        start = len / 2;
+    } else {
+        start = (len - 1) / 2 + 1;
+    }
+
+    for (i = str; str[i] != '\0'; i+=2) {
         _putchar(str[i]);
     }
     _putchar('\n');
--- pointers_arrays_strings/7-puts_half.c
+++ pointers_arrays_strings/7-puts_half.c
@@ -3,22 +3,25 @@
 #include <stdio.h>
 /**
  * main - check the code
- *
- * Return: Always 0.
+ * @str: variable
+ * puts_half : fonction
  */
-void puts_half(char *str){
+void puts_half(char *str)
+{
 
-    int len = 0;
-    int i;
-    int start;
-    while (str[len] != '\0') {
-        len++;
-    }
+int len = 0;
+int i;
+int start;
+while (str[len] != '\0')
+{
+len++;
+}
 
-     start = (len % 2 == 0) ? len / 2 : (len - 1) / 2 + 1;
+start = (len % 2 == 0) ? len / 2 : (len - 1) / 2 + 1;
 
-    for (i = start; str[i] != '\0'; i++) {
-        _putchar(str[i]);
-    }
-    _putchar('\n');
+for (i = start; str[i] != '\0'; i++)
+{
+_putchar(str[i]);
+}
+_putchar('\n');
 }
