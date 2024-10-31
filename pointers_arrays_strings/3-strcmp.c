38c4477d76458701f54573ca56ce44c59cee453c
aurelien <aurelienmest@outlook.fr>
2024-10-31 17:28:12 +0100

    chal8

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -10,18 +10,14 @@
 
 int _strcmp(char *s1, char *s2) 
 {
-	int result = strcmp(s1, s2);
-
-	if (result == 0) 
-	{
-		return 0;
-    } 
-		else if (result < 0) 
-	{
-		return -15;
-   	} 
-   	else 
-   	{
-		return 15;
-	}
+    while (*s1 && *s2)
+    {
+        if (*s1 != *s2)
+        {
+            return (*s1 - *s2);
+        }
+        s1++;
+        s2++;
+    }
+    return (*s1 - *s2);
 }
