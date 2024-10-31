6ffba10df845ee51e73bd1fd7509fc6e5ae2c574
aurelien <aurelienmest@outlook.fr>
2024-10-31 14:31:20 +0100

    chal8

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -3,21 +3,23 @@
 
 /**
  * main - check the code
- *
+ * _strcat : fonction
  * Return: Always 0.
  */
 char *_strcat(char *dest, char *src)
 {
-    
- char *ptr = dest;
-while (*ptr != '\0') {
-    ptr++;
+char *ptr = dest;
+
+while (*ptr != '\0')
+{
+ptr++;
 }
- while (*src != '\0') {
-        *ptr = *src;
-        ptr++;      
-        src++;       
-    }
- *ptr = '\0';
- return dest;
+while (*src != '\0')
+{
+*ptr = *src;
+ptr++;
+src++;
+}
+*ptr = '\0';
+return (dest);
 }
--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -8,7 +8,6 @@
  */
 void swap_int(int *a, int *b)
 {
-
 int temps;
 temps = *a;
 *a = *b;
