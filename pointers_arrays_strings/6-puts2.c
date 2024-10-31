0eac2f44d7cf0ed85a7470492b590291d4e78c61
aurelien <aurelienmest@outlook.fr>
2024-10-31 09:28:40 +0100

    chal8

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -6,8 +6,13 @@
  * Return: Always 0.
  */
 void puts2(char *str) {
+    int len = 0;
     int i;
-    for (i = 0; str[i] != '\0'; i += 2) {
+    while (str[len] != '\0') {
+        len++;
+    }
+
+    for (i = 0; str[i] != '\0'; i+=2) {
         _putchar(str[i]);
     }
     _putchar('\n');
