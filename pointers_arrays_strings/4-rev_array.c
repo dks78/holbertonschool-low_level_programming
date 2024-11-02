3958b549c0c590ed7a33d15177bad336c9404963
aurelien <aurelienmest@outlook.fr>
2024-11-02 17:57:02 +0100

    chal8

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_array - print an array of integers
+ * @a: an array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: nothing.
+ */
+void reverse_array(int *a, int n)
+{
+    int temp;
+    int i;
+
+    /*Inverser le tableau en utilisant un swa*/ 
+    for (i = 0; i < n / 2; i++)
+    {
+        temp = a[i];
+        a[i] = a[n - 1 - i];
+        a[n - 1 - i] = temp;
+    }
+}
+ 
--- pointers_arrays_strings/main.c
+++ pointers_arrays_strings/main.c
@@ -8,34 +8,28 @@
  */
 int main(void)
 {
-    char s1[98];
-    char *ptr;
+    int a[] = {0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 98, 1024, 1337};
+
+    print_array(a, sizeof(a) / sizeof(int));
+    reverse_array(a, sizeof(a) / sizeof(int));
+    print_array(a, sizeof(a) / sizeof(int));
+    return (0);
+}
+
+void print_array(int *a, int n) /* N = au nombre de case du tableau , *a est le pointeur du tableau a[] */
+{
     int i;
 
-    for (i = 0; i < 98 - 1; i++)
+    i = 0;
+    while (i < n) /* la boucle continue tan que i es inférieur a n , n correspond au nombre de case du tableau , donc la boucle continura jusqua avoir parcourue chaque case */
     {
-        s1[i] = '*';
-    }
-    s1[i] = '\0';
-    printf("%s\n", s1);
-    ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 5);
-    printf("%s\n", s1);
-    printf("%s\n", ptr);
-    ptr = _strncpy(s1, "First, solve the problem. Then, write the code\n", 90);
-    printf("%s", s1);
-    printf("%s", ptr);
-    for (i = 0; i < 98; i++)
-    {
-        if (i % 10)
+        if (i != 0) /* is i est différent de 0 affiche une virgule , effectivement 0 et le debut de la boucle donc aucune valeur sera afficher 
+        i est un entier qui s'incrémente a chaque itinération du tableau , comme elle recomance 1 fois affiche une virgule 2 fois affiche une virgule , premiere lancer i = 1 donc affiche un tableau */
         {
-            printf(" ");
+            printf(", ");
         }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", s1[i]);
+        printf("%d", a[i]); /*/*/
+        i++;
     }
     printf("\n");
-    return (0);
 }
