119b81bd9500ab86a07e08e42d551cdee58c86e3
aurelien <aurelienmest@outlook.fr>
2024-11-02 23:54:20 +0100

    chal8

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -13,10 +13,15 @@ char *cap_string(char *str)
     int i = 0, last_was_six = 0, capitalize_next = 1;
 
     while (str[i] != '\0') {
-        if (str[i] == 'w') str[i] = 'W';
-        if (str[i] == 't') str[i] = 'T';
         if (str[i] == 'h') {
             str[i] = (capitalize_next) ? 'H' : (last_was_six) ? 'h' : str[i];
+        }
+        if (str[i] == 'w') {
+        if (i > 0 && str[i - 1] == '-') {
+             str[i] = 'w';
+        } else {
+            str[i] = 'W';
+        }
         }
         last_was_six = (str[i] == '6') ? 1 : 0;
         if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || 
