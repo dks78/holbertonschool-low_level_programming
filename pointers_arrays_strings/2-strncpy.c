483047c68809d5a0f0fedba7bbf7c699a54bc7ab
aurelien <aurelienmest@outlook.fr>
2024-11-02 15:13:50 +0100

    chal8

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -3,19 +3,22 @@
 
 /**
  * main - check the code
- *
+ * @dest - varibale
  * Return: Always 0.
- */
-char *_strncpy(char *dest, char *src, int n) {
-    int i;
+*/
+char *_strncpy(char *dest, char *src, int n)
+{
+int i;
 
-    for (i = 0; i < n && src[i] != '\0'; i++) {
-        dest[i] = src[i];
-    }
-    
-    for (; i < n; i++) {
-        dest[i] = '\0';
-    }
+for (i = 0; i < n && src[i] != '\0'; i++)
+{
+dest[i] = src[i];
+}
 
-    return dest;
+for (; i < n; i++)
+{
+dest[i] = '\0';
+}
+
+return (dest);
 }
