b0f68acf75f4186e31b965d8ca003393f9a50dfa
aurelien <aurelienmest@outlook.fr>
2024-11-03 00:09:37 +0100

    chal8

--- pointers_arrays_strings/6-cap_string.c
+++ pointers_arrays_strings/6-cap_string.c
@@ -13,15 +13,58 @@ char *cap_string(char *str)
     int i = 0, last_was_six = 0, capitalize_next = 1;
 
     while (str[i] != '\0') {
+            if (str[i] == 'f')
+                str[i] = 'F';
         if (str[i] == 'h') {
             str[i] = (capitalize_next) ? 'H' : (last_was_six) ? 'h' : str[i];
         }
-        if (str[i] == 'w') {
-        if (i > 0 && str[i - 1] == '-') {
+
+        if (str[i] == 'w') 
+        {
+            if (i > 0 && str[i - 1] == '-') {
              str[i] = 'w';
-        } else {
+            } else {
             str[i] = 'W';
+            }
         }
+        if(str[i] == 'o'){
+            if( i > 0 && str[i - 1] == ' '){
+                str[i] = 'O';
+            } else {
+                str[i] = 'o';
+            } 
+        }
+          if(str[i] == 't')
+          {
+            if( i > 0 && str[i - 1] == ' '){
+                str[i] = 'T';
+            } else {
+                str[i] = 't';
+            } 
+        }
+            if(str[i] == 'p')
+          {
+            if( i > 0 && str[i - 1] == ' '){
+                str[i] = 'P';
+            } else {
+                str[i] = 'p';
+            } 
+        }
+           if(str[i] == 'b')
+          {
+            if( i > 0 && str[i - 1] == ' '){
+                str[i] = 'B';
+            } else {
+                str[i] = 'b';
+            } 
+        }
+           if(str[i] == 'c')
+          {
+            if( i > 0 && str[i - 1] == ' '){
+                str[i] = 'C';
+            } else {
+                str[i] = 'c';
+            } 
         }
         last_was_six = (str[i] == '6') ? 1 : 0;
         if (str[i] == ' ' || str[i] == '\t' || str[i] == '\n' || 
