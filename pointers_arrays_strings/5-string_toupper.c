b82b8e48ddc90bc9dab24e23f6c803344c445bfc
aurelien <aurelienmest@outlook.fr>
2024-11-02 19:57:47 +0100

    chal8

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -2,19 +2,23 @@
 #include <stdio.h>
 /**
  * main - check the code
+ * string_toupper - fonction to change character minuscule a majuscule
+ * @str : pointeur
  *
- * Return: Always 0.
- */
+ *
+ *
+ *
+*/
 char *string_toupper(char *str)
 {
-    int i = 0;
-     while (str[i] != '\0')
-    {
 
-        if (str[i]  >= 97 &&  str[i] <= 122)
-             
-            str[i] = str[i] - 32;
-        i++;
-    }
-    return str;
+int i = 0;
+while (str[i] != '\0')
+{
+
+if (str[i]  >= 97 &&  str[i] <= 122)
+str[i] = str[i] - 32;
+i++;
+}
+return (str);
 }
