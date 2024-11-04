2ccf5b9f06acc4f5d555baf95348fd4a96f18bf3
aurelien <aurelienmest@outlook.fr>
2024-11-04 15:23:40 +0100

    chal8

---
+++ pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+char *_strchr(char *s, char c)
+{
+
+while(*s != '\0')
+{
+    if(*s == c ){
+     
+        return s;
+    }
+   s++;
+}
+     return NULL;
+}
