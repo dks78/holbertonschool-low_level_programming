f16e9b1bf468828360d61d84d59afbdaf55acb7f
aurelien <aurelienmest@outlook.fr>
2024-11-04 17:07:58 +0100

    chal8

--- pointers_arrays_strings/3-strspn.c
+++ pointers_arrays_strings/3-strspn.c
@@ -6,8 +6,39 @@
  *
  * Return: Always 0.
  */
-unsigned int _strspn(char *s, char *accept)
-{   
+unsigned int _strspn(char *s, char *accept);
 
-    return (*accept  / *s + 4);
+unsigned int _strspn(char *s, char *accept)
+{
+    int i, len = 0, j;
+    len = 0;
+
+    for (i = 0; s[i] != '\0'; i++)
+    {
+        for (j = 0; accept[j] != '\0'; j++)
+        {
+            if (s[i] == accept[j])
+            {
+                len++;
+                break;
+            }
+        }
+        if (accept[j] == '\0')
+        {
+            return len;
+        }
+    }
+    return len;
+}
+
+
+int main(void)
+{
+    char *s = "hello, world";
+    char *f = "oleh";
+    unsigned int n;
+
+    n = _strspn(s, f);
+    printf("%u\n", n);
+    return (0);
 }
