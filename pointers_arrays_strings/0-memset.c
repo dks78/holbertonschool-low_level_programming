50fd1192f6ca381817375f8ea6ca23da47011d3e
aurelien <aurelienmest@outlook.fr>
2024-11-04 12:07:38 +0100

    chal8

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -2,7 +2,13 @@
 #include "main.h"
 #include <stdio.h>
 
-
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
 char *_memset(char *s, char b, unsigned int n)
 {
 
@@ -12,7 +18,5 @@ char *_memset(char *s, char b, unsigned int n)
     {
         s[i] = b;
     }
-    
-   _putchar('\n');
     return s;
 }
