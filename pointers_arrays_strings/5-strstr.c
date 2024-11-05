cca747fbbc2063191626fe5c92f709db2dbf94a2
aurelien <aurelienmest@outlook.fr>
2024-11-05 14:21:30 +0100

    chal8

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -1,22 +1,33 @@
+
 #include "main.h"
-#include <stdio.h>
-#include <string.h>
+#include <stddef.h>
 /**
- * main - check the code
+ * _strstr - Localise une sous-chaîne dans une chaîne
+ * @haystack: la chaîne principale dans laquelle chercher
+ * @needle: la sous-chaîne à trouver dans haystack
  *
- * Return: Always 0.
+ * Description: Cette fonction cherche la première occurrence de la sous-chaîne
+ * needle dans la chaîne haystack. Les octets de fin '\0' ne sont pas comparés.
+ * Retourne un pointeur vers le début de la sous-chaîne trouvée, ou NULL
+ * si la sous-chaîne n'est pas trouvée.
+ *
+ * Return: pointeur vers le début de needle dans haystack, ou NULL si non trouvé
  */
 char *_strstr(char *haystack, char *needle)
 {
-    int i, j;
-    if(*needle == '\0')
-    {
-        return haystack;    
-    }
-    for (i = 0; haystack[i] != '\0'; i++) {
-        for (j = 0; needle[j] != '\0' && haystack[i + j] == needle[j]; j++) {
-            return &haystack[i];
-        }
-    }
-    return NULL;
+int i, j;
+if (*needle == '\0')
+return (haystack);
+for (i = 0; haystack[i] != '\0'; i++)
+{
+if (haystack[i] == needle[0])
+{
+for (j = 0; needle[j] != '\0' && haystack[i + j] == needle[j]; j++)
+{
+}
+if (needle[j] == '\0')
+return &haystack[i];
+}
+}
+return (NULL);
 }
