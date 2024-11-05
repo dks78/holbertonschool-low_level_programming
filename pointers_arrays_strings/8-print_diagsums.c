9f26d48376ef6205f0f52b4e56bdad0b58525c0b
aurelien <aurelienmest@outlook.fr>
2024-11-05 16:16:06 +0100

    chal8

--- pointers_arrays_strings/8-print_diagsums.c
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -9,12 +9,12 @@
 void print_diagsums(int *a, int size)
 {
    int sum1 = 0, sum2 = 0;
+    int i;
 
-
-    for (int i = 0; i < size; i++) {
+    for (i = 0; i < size; i++) {
         sum1 += a[i * size + i];
     }
-       for (int i = 0; i < size; i++) {
+       for ( i = 0; i < size; i++) {
         sum2 += a[i * size + (size - i - 1)];
     }
 
