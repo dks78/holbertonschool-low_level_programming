116d4c121c21a919d35c9e1108b5e6680ab2f1bc
aurelien <aurelienmest@outlook.fr>
2024-11-05 14:25:17 +0100

    chal8

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -6,30 +6,17 @@
  *
  * Return: Always 0.
  */
- 
-char *_strpbrk(char *s, char *accept);
 char *_strpbrk(char *s, char *accept)
 {
     int i, j;
 
-    for (i = 0; s[i] != '\0'; i++) { /*parcour la premiére chaine de charactére qui est S */
-        for (j = 0; accept[j] != '\0'; j++) { /*parcour la dexiéme chaine de charactére*/
-            if (s[i] == accept[j]) { /*affcihe les charactére qui son identique dans les 2 chaine , le h e n'apparait 
-            pas dans la dexiéme chaine donc cela ne va pas etre prit en comtpe */
+    for (i = 0; s[i] != '\0'; i++) {
+        for (j = 0; accept[j] != '\0'; j++) {
+            if (s[i] == accept[j]) { 
                 return &s[i];
             }
         }
     }
     
     return NULL;
-}
-int main(void)
-{
-    char *s = "hello, world";
-    char *f = "world";
-    char *t;
-
-    t = _strpbrk(s, f);
-    printf("%s\n", t);
-    return (0);
 }
