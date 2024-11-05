b578c0ee1412596476763d4d1b56e1c38cf5c97f
aurelien <aurelienmest@outlook.fr>
2024-11-05 16:14:31 +0100

    chal8

---
+++ pointers_arrays_strings/8-print_diagsums.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+void print_diagsums(int *a, int size)
+{
+   int sum1 = 0, sum2 = 0;
+
+
+    for (int i = 0; i < size; i++) {
+        sum1 += a[i * size + i];
+    }
+       for (int i = 0; i < size; i++) {
+        sum2 += a[i * size + (size - i - 1)];
+    }
+
+
+    printf("%d, %d\n", sum1, sum2);
+}
+
+
+
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -28,4 +28,5 @@ char *_strstr(char *haystack, char *needle);
 int compare(const char *X, const char *Y);
 char *_memset(char *s, char b, unsigned int n);
 void print_chessboard(char (*a)[8]);
+void print_diagsums(int *a, int size);
 #endif
