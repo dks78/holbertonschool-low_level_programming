6bf7d3df6562ffe0b7c0a63f900b0a2a8dc7119c
aurelien <aurelienmest@outlook.fr>
2024-11-06 11:58:13 +0100

    chal8

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -1,17 +1,17 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- * main - check the code
+ * _strlen_recursion - retourne la longueur d'une chaîne de caractères
+ * @s: pointeur vers la chaîne de caractères
  *
- * Return: Always 0.
+ * Retourne: la longueur de la chaîne
  */
-
 int _strlen_recursion(char *s)
 {
+if (*s == '\0')
 
-    if (*s == '\0')
-        return 0;
+return (0);
 
-    return 1 + _strlen_recursion(s + 1); 
+return (1 + _strlen_recursion(s + 1));
+
 }
