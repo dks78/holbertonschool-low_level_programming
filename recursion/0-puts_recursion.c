b8fcba49cbb2d127c725a1361b7ce67ad9e7f032
aurelien <aurelienmest@outlook.fr>
2024-11-06 11:00:19 +0100

    chal8

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -1,16 +1,21 @@
 #include "main.h"
 #include <stdio.h>
 /**
- * _puts_recursion - affiche une chaîne de caractères suivie d'une nouvelle ligne
+ * _puts_recursion - affiche une chaîne de caractères + une nouvelle ligne
  * @s: pointeur vers la chaîne de caractères à afficher
  */
 void _puts_recursion(char *s)
+
 {
-    if(*s == '\0'){
-      _putchar('\n');
-      return;
-    }
-    
-    _putchar(*s);
-    _puts_recursion(s + 1);
+
+if (*s == '\0')
+
+{
+_putchar('\n');
+return;
+}
+
+_putchar(*s);
+_puts_recursion(s + 1);
+
 }
