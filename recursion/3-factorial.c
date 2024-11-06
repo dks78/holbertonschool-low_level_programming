5cbbc1db3bb0713f5ad5b6ec3083857e9e15bcc0
aurelien <aurelienmest@outlook.fr>
2024-11-06 12:16:17 +0100

    chal8

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -1,21 +1,24 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- * main - check the code
+ * factorial - Calcule le factoriel d'un nombre entier non négatif
+ * @n: Le nombre entier pour lequel le factoriel est calculé
  *
- * Return: Always 0.
+ * Description: Cette fonction calcule le factoriel d'un nombre entier positif.
+ * Si le nombre est négatif, la fonction retourne -1 pour indiquer une erreur,
+ * car le factoriel n'est pas défini pour les entiers négatifs.
+ *
+ * Return: Le factoriel de n si n >= 0, sinon -1 pour indiquer une erreur
  */
 int factorial(int n)
 {
-    
-   if (n < 0)
-    {
-        return -1;
-    }
-    if (n == 0 || n == 1) 
-    {
-        return 1;
-    }
-    return n * factorial(n - 1);
+if (n < 0)
+{
+return (-1);
+}
+if (n == 0 || n == 1)
+{
+return (1);
+}
+return (n * factorial(n - 1));
 }
