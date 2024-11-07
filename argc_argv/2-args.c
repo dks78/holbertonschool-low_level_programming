fc541401d108acaed20a8499652c71096cc78545
aurelien <aurelienmest@outlook.fr>
2024-11-07 03:59:40 +0100

    chal8

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,16 +1,23 @@
 #include <stdio.h>
 
+/**
+ * main => Affiche tous les arguments reçus ppour
+ * @argc: Le nombre d'arguments
+ * @argv: Le tableau des arguments
+ *
+ * Retourne : Toujours 0 (succès)
+ */
 int main(int argc, char *argv[])
 {
-    int count;
-    printf("%s\n", argv[0]);
-  if (argc > 1) 
-    { 
-      for (count = 1; count < argc; count++) 
-	{
-	printf("%s\n", argv[count]); /*va affichez tous les arguments qu'on lui donne */
-	} 
-} 
-  return 0; 
+int count;
+printf("%s\n", argv[0]);
+if (argc > 1)
+{
+for (count = 1; count < argc; count++)
+{
+printf("%s\n", argv[count]);
+}
+}
+return (0);
 }
 
