8cc651b14c234dc3c8169a87ec0b6337b486e9ef
aurelien <aurelienmest@outlook.fr>
2024-11-07 03:35:21 +0100

    chal8

--- argc_argv/1-args.c
+++ argc_argv/1-args.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
+/**
+ * main : Affiche le nombre d'arguments passés au programme
+ * @argc: Le nombre d'arguments passés au programme
+ * @argv: Un tableau contenant les arguments passés au programme
+ *
+ * Retourne: Toujours 0 (indique que le programme s'est exécuté avec succès)
+*/
 
-int main (int argc, char *argv[]) 
-{ 
-    (void)argv;
-    printf("%d\n", argc - 1);
-    return 0;
+int main(int argc, char *argv[])
+{
+(void)argv;
+
+printf("%d\n", argc - 1);
+
+return (0);
 }
