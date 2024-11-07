4fc0a558decae6c9e798fd427b6a0b89714fbf86
aurelien <aurelienmest@outlook.fr>
2024-11-07 03:57:24 +0100

    chal8

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -2,12 +2,13 @@
 
 int main(int argc, char *argv[])
 {
-    int count; 
+    int count;
+    printf("%s\n", argv[0]);
   if (argc > 1) 
     { 
       for (count = 1; count < argc; count++) 
-	{ 
-	  printf(" %s\n", argv[count]); /*va affichez tous les arguments qu'on lui donne */
+	{
+	printf("%s\n", argv[count]); /*va affichez tous les arguments qu'on lui donne */
 	} 
 } 
   return 0; 
