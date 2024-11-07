4a7aaa20c36bc2bb087fe70356df6fddef33565d
aurelien <aurelienmest@outlook.fr>
2024-11-07 02:56:49 +0100

    chal8

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,8 +1,18 @@
 #include <stdio.h>
 
-int main (int argc, char *argv[]) 
-{ 
-    (void)argc;
-  printf("%s\n",argv[0]); 
-  return 0;
+/**
+ * main : fuction
+ *
+ * @argc: Le nombre d'arguments passés au programme
+ * @argv: Un tableau contenant les arguments passés au programme
+ *
+ * Retourne : Toujours 0 (succès)
+ */
+int main(int argc, char *argv[])
+{
+(void)argc;/* Ignorer le paramètre argc */
+
+printf("%s\n", argv[0]);/* Afficher le nom du programme */
+
+return (0);/* Retourner 0 pour indiquer un succès */
 }
