5f73e2190c85de62a1f2fee170863cb2403a0416
aurelien <aurelienmest@outlook.fr>
2024-11-07 03:40:27 +0100

    chal8

--- argc_argv/2-args.c
+++ argc_argv/2-args.c
@@ -1,2 +1,16 @@
 #include <stdio.h>
 
+int main(int argc, char *argv[])
+{
+(void)argc;
+
+printf("%s\n", argv[0]);
+printf("%s\n", argv[1]);
+printf("%s\n", argv[2]);
+printf("%s\n", argv[3]);
+printf("%s\n", argv[4]);
+printf("%s\n", argv[5]);
+printf("%s\n", argv[6]);
+printf("%s\n", argv[7]);
+return (0);
+}
