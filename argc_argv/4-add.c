fa61650df14f042a8a17b0f256527653297cda21
aurelien <aurelienmest@outlook.fr>
2024-11-07 15:25:39 +0100

    chal8

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>
+#include <ctype.h>
 /**
  * main : Affiche le nombre d'arguments passés au programme
  * @argc: Le nombre d'arguments passés au programme
@@ -14,21 +14,29 @@ int main(int argc, char *argv[])
     int i;
     int cpt = 1;
 
-    if(argc < 0)
+    if (argc == 1)
     {
-        printf("Error\n");
-        return 1;
+        printf("0\n");
+        return (0);
     }
 
     for (i = 1; i < argc; i++)
     {
+        for (int j = 0; argv[i][j] != '\0'; j++)
+        {
+            if (argv[i][j] < '0' || argv[i][j] > '9')
+            {
+                printf("Error\n");
+                return (1);
+            }
+        }
 
         int c = atoi(argv[i]);
         cpt += c;
-
     }
 
     printf("%d\n", cpt - 1);
 
-return (0);
+    return (0);
 }
+
