413f4d7dd3965be796c0844d6ae4119467137f5e
aurelien <aurelienmest@outlook.fr>
2024-11-12 11:15:17 +0100

    chal8

--- malloc_free/main.c
+++ malloc_free/main.c
@@ -14,43 +14,17 @@ Retourne NULL si size = 0
 Retourne un pointeur vers le tableau ou NULL en cas d'échec
  * Return: Nothing.
  */
-void simple_print_buffer(char *buffer, unsigned int size)
-{
-    unsigned int i;
-
-    i = 0;
-    while (i < size)
-    {
-        if (i % 10)
-        {
-            printf(" ");
-        }
-        if (!(i % 10) && i)
-        {
-            printf("\n");
-        }
-        printf("0x%02x", buffer[i]);
-        i++;
-    }
-    printf("\n");
-}
-
-/**
- * main - check the code
- *
- * Return: Always 0.
- */
 int main(void)
 {
-    char *buffer;
+    char *s;
 
-    buffer = create_array(98, 'H');
-    if  (buffer == NULL)
+    s = _strdup("Duplicated");
+    if (s == NULL)
     {
         printf("failed to allocate memory\n");
         return (1);
     }
-    simple_print_buffer(buffer, 98);
-    free(buffer);
+    printf("%s\n", s);
+    free(s);
     return (0);
 }
