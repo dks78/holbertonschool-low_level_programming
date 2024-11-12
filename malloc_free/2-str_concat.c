3a51935bc76acd4bd1c5ea4a07dd6d13f64f6c53
aurelien <aurelienmest@outlook.fr>
2024-11-12 14:27:55 +0100

    chal8

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -24,7 +24,11 @@ char *str_concat(char *s1, char *s2)
         longuer2++;
     }
 
-   result = malloc((longeur + longuer2 + 1) * sizeof(char));
+   result = malloc((longeur + longuer2 + 1)); /*
+    La taille allouée est la somme des longueurs de s1 et s2, plus 1 s
+    upplémentaire pour le caractère de fin de chaîne '\0'.
+    allouer (longeur + longuer2 + 1) octets.
+   */
     if(result == NULL)
     {
         return NULL;
@@ -33,10 +37,16 @@ char *str_concat(char *s1, char *s2)
     for ( i = 0; i < longeur ; i++)
     {
         result[i] = s1[i];
-    }
+    } /*Cette boucle copie tous les caractères de s1 dans result. Vous commencez à copier à partir de l'index 0 et continuez jusqu'à la fin de s1 (l'index longeur - 1).
+        À la fin de cette boucle, la première partie de result contient la chaîne s1.*/
     for (i = 0; i < longuer2; i++)
     {
-        result[longeur + i] = s2[i];
+        result[longeur + i] = s2[i]; /*
+        Cette boucle copie tous les caractères de 
+        s2 à la suite de s1 dans result. Les caractères de s2 sont copiés à partir de l'index longeur dans result, donc après la fin de la chaîne s1.
+        Pourquoi longeur + i ? Parce que les premiers longeur 
+        indices de result contiennent déjà la chaîne s1. Ensuite, nous ajoutons les caractères de s2 après.
+        */
     }
 
 
