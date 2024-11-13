8ee5de23526fdd2304ddc7772d61a49f13549e11
aurelien <aurelienmest@outlook.fr>
2024-11-13 15:20:51 +0100

    chal8

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * simple_print_buffer - prints buffer in hexa
+ * @buffer: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+    void *ptr;
+    if(nmemb == 0 || size == 0)
+    {
+        return NULL;
+    }
+    ptr = malloc(nmemb * size);
+    if(ptr == NULL)
+    {
+        return NULL;
+    }
+    return ptr;
+}
+
