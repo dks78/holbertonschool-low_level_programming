e6cb29fd1dd703a36edea7d240b18e81ea5c257c
aurelien <aurelienmest@outlook.fr>
2024-11-13 02:47:30 +0100

    chal8

--- malloc_free/main.c
+++ malloc_free/main.c
@@ -2,7 +2,14 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-
+/**
+ * print_grid - prints a grid of integers
+ * @grid: the address of the two dimensional grid
+ * @width: width of the grid
+ * @height: height of the grid
+ *
+ * Return: Nothing.
+ */
 void print_grid(int **grid, int width, int height)
 {
     int w;
@@ -41,5 +48,6 @@ int main(void)
     grid[0][3] = 98;
     grid[3][4] = 402;
     print_grid(grid, 6, 4);
+    free_grid(grid, 4);
     return (0);
 }
