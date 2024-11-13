0abee2d1628ddde37d02adf8601d41af7c28cf74
aurelien <aurelienmest@outlook.fr>
2024-11-13 02:51:35 +0100

    chal8

--- malloc_free/4-free_grid.c
+++ malloc_free/4-free_grid.c
@@ -7,16 +7,19 @@
 /**
  * print_grid - prints a grid of integers
  * @grid: the address of the two dimensional grid
- * @width: width of the grid
+ * free_grid : fonction to libére memoire the pointer of array
+
  * @height: height of the grid
  *
  * Return: Nothing.
  */
+void free_grid(int **grid, int height)
+{
+int i;
 
-void free_grid(int **grid, int height) { 
-    int i;
-    for ( i = 0; i < height; i++) {
-         free(grid[i]); /*libére la mémoir tu tableau de pointeur */
+for (i = 0; i < height; i++)
+{
+free(grid[i]); /*libére la mémoir tu tableau de pointeur */
 
 }
 free(grid);
