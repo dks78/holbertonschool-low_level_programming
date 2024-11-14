63d11cf1f883a37605a8b21efb0e1a15f93d6130
aurelien <aurelienmest@outlook.fr>
2024-11-14 16:47:16 +0100

    chal8

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -8,12 +8,13 @@
  * @owner: Le propriétaire du chien (chaîne de caractères)
  */
 
+
 typedef struct dog
 {
 char *name;
 float age;
 char *owner;
-} dog;
+} dog_t;
 
  void print_dog(struct dog *d);
  void init_dog(struct dog *d, char *name, float age, char *owner);
