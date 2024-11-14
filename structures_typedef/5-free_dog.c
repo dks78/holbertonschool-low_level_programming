8ce0238f15e0ac33653be24c8be5317c122041a1
aurelien <aurelienmest@outlook.fr>
2024-11-14 17:16:36 +0100

    chal8

---
+++ structures_typedef/5-free_dog.c
@@ -0,0 +1,13 @@
+#include <stdlib.h>
+#include <string.h>
+#include "dog.h"
+
+void free_dog(dog_t *d){
+{
+    if (d != NULL) {
+        free(d->name);
+        free(d->owner);
+        free(d);
+    }
+}
+}
