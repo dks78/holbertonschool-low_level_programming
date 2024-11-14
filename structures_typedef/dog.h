e218e3a33d21d449e8d62c1d4aa85099b9ee5cb9
aurelien <aurelienmest@outlook.fr>
2024-11-14 12:29:35 +0100

    chal8

---
+++ structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - Structure représentant un chien
+ * @name: Le nom du chien (chaîne de caractères)
+ * @age: L'âge du chien (nombre flottant)
+ * @owner: Le propriétaire du chien (chaîne de caractères)
+ */
+typedef struct dog
+{
+    char *name;
+    float age;
+    char *owner; 
+} dog;
+
+#endif
