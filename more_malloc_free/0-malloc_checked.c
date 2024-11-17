3ff8a88ae61abf9ad674dbc3300ab55d0e362c27
aurelien <aurelienmest@outlook.fr>
2024-11-17 19:28:12 +0100

    chal8

--- more_malloc_free/0-malloc_checked.c
+++ more_malloc_free/0-malloc_checked.c
@@ -1,11 +1,19 @@
 #include "main.h"
 #include <stdlib.h>
 
-
-void *malloc_checked(unsigned int b) {
-    void *ptr = malloc(b); /*Une simple allocation mémoire*/
-    if (ptr == NULL) {
-        exit(98);
-    }
-    return ptr;
+/**
+ * malloc_checked - Alloue de la mémoire en utilisant malloc
+ * @b: Taille de la mémoire à allouer en octets
+ *
+ * Return: Pointeur vers la mémoire allouée
+ * Si l'allocation échoue, le programme termine avec un statut de 98
+ */
+void *malloc_checked(unsigned int b)
+{
+void *ptr = malloc(b); /* Une simple allocation mémoire */
+if (ptr == NULL)
+{
+exit(98);
+}
+return (ptr);
 }
