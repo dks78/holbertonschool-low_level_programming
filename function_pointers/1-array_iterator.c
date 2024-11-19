0a71363aca8f78b90c6ed3a9eb774717227d7f5a
aurelien <aurelienmest@outlook.fr>
2024-11-19 12:48:18 +0100

    chal8

--- function_pointers/1-array_iterator.c
+++ function_pointers/1-array_iterator.c
@@ -1,21 +1,25 @@
 #include <stdio.h>
 #include "function_pointers.h"
-
 /**
- * print_elem - prints an integer
- * @elem: the integer to print
+ * array_iterator - Exécute une fonction donnée surchaqueElémentD'unTableau.
+ * @array: Le tableau d'entiers à traiter.
+ * @size: La taille du tableau.
+ * @action: Pointeur vers une fonction à utiliser sur chaque élément.
  *
- * Return: Nothing.
+ * Description: Cette fonction parcourt chaque élément du tableau donné
+ * et applique la fonction pointée par @action à chaque élément.
+ * Si le tableau ou la fonction est NULL, la fonction retourne immédiatement.
  */
 void array_iterator(int *array, size_t size, void (*action)(int))
 {
-     size_t i; 
-      if (array == NULL || action == NULL) {
-        return;
-    }
-
-
-    for (i = 0; i < size; i++) {
-        action(array[i]);
-    }
+size_t i;
+if (array == NULL || action == NULL)
+{
+return;
 }
+for (i = 0; i < size; i++)
+{
+action(array[i]);
+}
+}
+
