4d76f2fbafaa5d5f51902374d30ef78c2c91ae66
aurelien <aurelienmest@outlook.fr>
2024-11-19 12:43:46 +0100

    chal8

--- function_pointers/0-print_name.c
+++ function_pointers/0-print_name.c
@@ -1,9 +1,16 @@
 #include <stdio.h>
-
-
-
+/**
+ * print_name - Exécute une fonction donnée sur un nom.
+ * @name: Le nom à utiliser comme argument de la fonction.
+ * @f: Pointeur vers une fonction qui prend un argument de type char *.
+ *
+ * Description: Cette fonction appelle la fonction pointée par @f,
+ * en lui passant @name comme argument. Cela permet d'exécuter une
+ * opération  définie par la fonction passée sur la chaîne @name.
+ */
 void print_name(char *name, void (*f)(char *))
 {
-    f(name);
+f(name);
 }
+
 
