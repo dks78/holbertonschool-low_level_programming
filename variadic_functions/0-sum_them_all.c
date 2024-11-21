1a8ce70a8efe8ec0cae3c46e499d27d63b657725
aurelien <aurelienmest@outlook.fr>
2024-11-21 12:58:26 +0100

    chal8

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -2,29 +2,34 @@
 #include <stdio.h>
 #include "variadic_functions.h"
 /**
- * main - check the code
+ * sum_them_all - Calcule la somme de tous ses paramètres.
+ * @n: Nombre d'arguments passés à la fonction.
  *
- * Return: Always 0.
+ * Description: Cette fonction utilise une liste d'arguments
+ * variables pour additionner tous les arguments de type int.
+ * Si n est égal à 0, la fonction retourne 0.
+ *
+ * Return: La somme des arguments ou 0 si n == 0.
  */
 int sum_them_all(const unsigned int n, ...)
-{   
-    unsigned int i;
-    int sum;
-    va_list ptr;
-    if( n == 0 )
-    {
-        return 0;
-    }
+{
+unsigned int i;
+int sum;
+va_list ptr;
 
+if (n == 0)
+{
+return (0);
+}
 
-    va_start(ptr, n);
+va_start(ptr, n);
 
-    for(i = 0 ; i < n; i++)
-    {
-        sum += va_arg(ptr, int);
-    }
-    va_end(ptr);
+for (i = 0 ; i < n; i++)
+{
+sum += va_arg(ptr, int);
+}
 
-    return sum;
+va_end(ptr);
 
+return (sum);
 }
