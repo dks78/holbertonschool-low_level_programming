9f36da67d78610661c69cb30049701b651ec4510
aurelien <aurelienmest@outlook.fr>
2024-12-03 18:21:19 +0100

    chal8

--- singly_linked_lists/1-list_len.c
+++ singly_linked_lists/1-list_len.c
@@ -3,16 +3,21 @@
 #include <stdio.h>
 #include "lists.h"
 
+/**
+ * list_len - Compte le nombre d'éléments dans une liste chaînée list_t.
+ * @h: Pointeur vers la tête de la liste list_t.
+ *
+ * Return: Le nombre de nœuds dans la liste.
+ */
 size_t list_len(const list_t *h)
 {
-   
-    int size = 0;
+size_t size = 0;
 
-        while (h != NULL)
-        {
-            ++size;
-            h = h ->next;
-        }
+while (h != NULL)
+{
+++size;
+h = h->next;
+}
 
-    return size;
+return (size);
 }
