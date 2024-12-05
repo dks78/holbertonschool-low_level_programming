8e909a3c49368f8ef2f464ccb4ff56e9da82dcb0
aurelien <aurelienmest@outlook.fr>
2024-12-05 10:24:14 +0100

    chal8

--- doubly_linked_lists/0-print_dlistint.c
+++ doubly_linked_lists/0-print_dlistint.c
@@ -1,22 +1,20 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * print_dlistint - Imprime tous les éléments d'une liste doublement chaînée
+ * print_dlistint - Imprime nles éléments d'une liste doublement chaînée
  * @h: Pointeur vers le premier élément de la liste.
  *
  * Return: Le nombre de nœuds imprimés.
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-    size_t count = 0;
-    const dlistint_t *temp = h;
-
-    while (temp != NULL)
-    {
-        printf("%d\n", temp->n);
-       
-        temp = temp->next;
-        count++;
-    }
-    return count;
+size_t count = 0;
+const dlistint_t *temp = h;
+while (temp != NULL)
+{
+printf("%d\n", temp->n);
+temp = temp->next;
+count++;
+}
+return (count);
 }
