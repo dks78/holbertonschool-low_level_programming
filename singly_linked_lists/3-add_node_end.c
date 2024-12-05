624c09f36d1ad7caf8214993e2cd09ef328da076
aurelien <aurelienmest@outlook.fr>
2024-12-05 09:45:29 +0100

    chal8

--- singly_linked_lists/3-add_node_end.c
+++ singly_linked_lists/3-add_node_end.c
@@ -1,4 +1,4 @@
-#include <stdlib.h>
+ #include <stdlib.h>
 #include <string.h>
 #include "lists.h"
 
@@ -9,27 +9,40 @@
  *
  * Return: Adresse du nouvel élément, ou NULL en cas d'échec.
  */
-
-list_t *add_node(list_t **head, const char *str)
+list_t *add_node_end(list_t **head, const char *str)
 {
-    list_t *new_node;
+    list_t *new_node, *temp;
+
 
     new_node = malloc(sizeof(list_t));
     if (new_node == NULL)
-    {
-        return NULL;
-    }
+        return (NULL);
+
 
     new_node->str = strdup(str);
     if (new_node->str == NULL)
     {
         free(new_node);
-        return NULL;
+        return (NULL);
     }
 
-    new_node->len = strlen(str);
-    new_node->next = *head;
-    *head = new_node;
 
-    return new_node;
+    new_node->len = strlen(str);
+    new_node->next = NULL;
+
+
+    if (*head == NULL)
+    {
+        *head = new_node;
+        return (new_node);
+    }
+
+    temp = *head;
+    while (temp->next != NULL)
+        temp = temp->next;
+
+
+    temp->next = new_node;
+
+    return (new_node);
 }
