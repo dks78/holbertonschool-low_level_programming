487f19921d4795cbda8132722989c88853e8ca8c
aurelien <aurelienmest@outlook.fr>
2024-12-05 09:54:29 +0100

    chal8

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -7,13 +7,12 @@
  */
 void free_list(list_t *head)
 {
-    list_t *temp;
-
-    while (head != NULL)
-    {
-        temp = head->next;  
-        free(head->str);
-        free(head);          
-        head = temp;      
-    }
+list_t *temp;
+while (head != NULL)
+{
+temp = head->next;
+free(head->str);
+free(head);
+head = temp;
+}
 }
