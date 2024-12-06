e4ce3f921d71743c7578fc070e6a5d22f53af579
aurelien <aurelienmest@outlook.fr>
2024-12-06 02:49:14 +0100

    chal8

--- doubly_linked_lists/4-free_dlistint.c
+++ doubly_linked_lists/4-free_dlistint.c
@@ -2,20 +2,16 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * add_dnodeint_end - Ajoute un nœud à la fin d'uliste doublementchaînée
- * @head: Pointeur vers le pointeur de la tête de la liste
- * @n: Données à insérer dans le nouveau nœud
- *
- * Return: Adresse du nouveau nœud ou NULL en cas d'échec
+ * free_dlistint - Libère une liste doublement chaînée
+ * @head: Pointeur vers la tête de la liste
  */
- void free_dlistint(dlistint_t *head)
+void free_dlistint(dlistint_t *head)
 {
-    dlistint_t  *temp;
-    while(head != NULL)
-    {
-        temp = head;
-        head = head ->next;
-        free(temp);
-    }
-
+dlistint_t  *temp;
+while (head != NULL)
+{
+temp = head;
+head = head->next;
+free(temp);
+}
 }
