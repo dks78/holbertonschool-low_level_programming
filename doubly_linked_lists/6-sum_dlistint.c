d5cee928f45e1210a6fcbc3c4677a0b3fb66116d
aurelien <aurelienmest@outlook.fr>
2024-12-06 12:15:18 +0100

    chal8

--- doubly_linked_lists/6-sum_dlistint.c
+++ doubly_linked_lists/6-sum_dlistint.c
@@ -2,24 +2,20 @@
 #include <stdio.h>
 #include "lists.h"
 /**
- * get_dnodeint_at_index - Récupère le nœud à uncertainindexd'une liste
- * @head: Pointeur vers le premier nœud de la liste doublement chaînée
- * @index: L'indice du nœud à récupérer, en commençant à 0
+ * sum_dlistint - Calcue la somme toutesles données d'une llement chaînée
+ * @head: Pointeur vers le premier nœud de la liste
  *
- * Return: Pointeur vers le nœud
+ * Return: La somme des valeurs de tous les nœuds,
  */
 int sum_dlistint(dlistint_t *head)
 {
+int some = 0;
 
- int somme = 0;
-
-    /* Parcourir la liste et additionner les valeurs */
-    while (head != NULL)
-    {
-        somme += head->n;  /* Ajouter la valeur du nœud courant */
-        head = head->next; /* Passer au nœud suivant */
-    }
-
-    return (somme);  /* */
+while (head != NULL)
+{
+somme += head->n;  /* Ajouter la valeur du nœud courant */
+head = head->next; /* Passer au nœud suivant */
+}
+return (somme);
 }
 
