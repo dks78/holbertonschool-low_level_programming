17348f26d2c2f0d9be8bbc3b9a5df28cdbb8dda5
aurelien <aurelienmest@outlook.fr>
2024-12-06 02:58:06 +0100

    chal8

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -1,16 +1,22 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include "lists.h"
-
+/**
+ * get_dnodeint_at_index - Récupère le nœud à uncertainindexd'une liste
+ * @head: Pointeur vers le premier nœud de la liste doublement chaînée
+ * @index: L'indice du nœud à récupérer, en commençant à 0
+ *
+ * Return: Pointeur vers le nœud
+ */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
-    unsigned int i = 0;
-    while (head != NULL)
-    {
-        if (i == index)
-            return head; 
-        head = head->next; 
-        i++;
-    }
-    return NULL;
+unsigned int i = 0;
+while (head != NULL)
+{
+if (i == index)
+return (head);
+head = head->next;
+i++;
+}
+return (NULL);
 }
