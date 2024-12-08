1d1b0eebeddcaf0c8cbcc4ed87218c0d6ec33d85
aurelien <aurelienmest@outlook.fr>
2024-12-08 23:00:30 +0100

    chal8

--- bit_manipulation/4-clear_bit.c
+++ bit_manipulation/4-clear_bit.c
@@ -9,9 +9,8 @@
  */
 int clear_bit(unsigned long int *n, unsigned int index)
 {
-    if (index >= 64)
-        return (-1);
-
-    *n &= ~(1UL << index);
-    return (1);
+if (index >= 64)
+return (-1);
+*n &= ~(1UL << index);
+return (1);
 }
