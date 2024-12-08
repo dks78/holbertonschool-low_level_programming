b335ca05dbdcdfbacca8c1a0ecc4a6bd7714914d
aurelien <aurelienmest@outlook.fr>
2024-12-08 23:30:27 +0100

    chal8

---
+++ file_io/1-create_file.c
@@ -0,0 +1,13 @@
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include "main.h"
+/**
+ * read_textfile - lit un fichier texte et l'affiche à la sortie standard.
+ * @filename: nom du fichier.
+ * @letters: nombre de lettres à lire et afficher.
+ * 
+ * Retourne le nombre de lettres lues et affichées, ou 0 en cas d'erreur.
+ */
+ 
