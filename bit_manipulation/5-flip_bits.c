1948ff0b94be5a0598992049a8e549b432be51fe
aurelien <aurelienmest@outlook.fr>
2024-12-08 23:03:56 +0100

    chal8

--- bit_manipulation/5-flip_bits.c
+++ bit_manipulation/5-flip_bits.c
@@ -1,23 +1,21 @@
 #include "main.h"
 
 /**
- * flip_bits - Renvoie le nombre de bits qu'il faut inverser pour passer de n à m.
+ * flip_bits - Renvoie le nombre dits qu'il faut inverser asser de n à m.
  * @n: Premier nombre.
- * @m: Deuxième nombre.
+ * @m: Deuxième nombr
  *
  * Return: Le nombre de bits à inverser.
  */
 unsigned int flip_bits(unsigned long int n, unsigned long int m)
 {
-    unsigned long int xor_result = n ^ m;
-    unsigned int count = 0;
-
-
-    while (xor_result)
-    {
-        count += (xor_result & 1);
-        xor_result >>= 1;           
-    }
-
-    return count;
+unsigned long int xor_result = n ^ m;
+unsigned int count = 0;
+while (xor_result)
+{
+count += (xor_result & 1);
+xor_result >>= 1;
 }
+return (count);
+}
+
