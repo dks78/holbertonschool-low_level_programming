66aa87438535b87067b3ccb7b00262b0258bd473
aurelien <aurelienmest@outlook.fr>
2024-12-08 22:54:27 +0100

    chal8

--- bit_manipulation/2-get_bit.c
+++ bit_manipulation/2-get_bit.c
@@ -9,8 +9,8 @@
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
-    if (index >= 64)
-        return (-1);
+if (index >= 64)
+return (-1);
 
-    return ((n >> index) & 1);
+return ((n >> index) & 1);
 }
