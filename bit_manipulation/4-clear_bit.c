24eb399c639a3d6f225f6993da30a992426c8b15
aurelien <aurelienmest@outlook.fr>
2024-12-08 22:59:56 +0100

    chal8

---
+++ bit_manipulation/4-clear_bit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * clear_bit - Définit un bit à 0 à un indice donné.
+ * @n: Pointeur vers le nombre dont le bit doit être modifié.
+ * @index: L'indice du bit à définir à 0.
+ *
+ * Return: 1 si la mise à jour a réussi, -1 en cas d'erreur.
+ */
+int clear_bit(unsigned long int *n, unsigned int index)
+{
+    if (index >= 64)
+        return (-1);
+
+    *n &= ~(1UL << index);
+    return (1);
+}
