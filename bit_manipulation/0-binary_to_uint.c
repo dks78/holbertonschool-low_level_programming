c84c6b340409c6fa5219c41d50c6ea5d7dc410b8
aurelien <aurelienmest@outlook.fr>
2024-12-08 22:35:01 +0100

    chal8

--- bit_manipulation/0-binary_to_uint.c
+++ bit_manipulation/0-binary_to_uint.c
@@ -9,20 +9,19 @@
  */
 unsigned int binary_to_uint(const char *b)
 {
-    unsigned int result = 0;
-    int i;
+unsigned int result = 0;
+int i;
 
-    if (b == NULL)
-        return (0);
+if (b == NULL)
+return (0);
 
-    for (i = 0; b[i] != '\0'; i++)
-    {
-        if (b[i] != '0' && b[i] != '1')
-            return (0);
+for (i = 0; b[i] != '\0'; i++)
+{
+if (b[i] != '0' && b[i] != '1')
+return (0);
+/* Décalage de résultat à gauche et ajout du bit actuel */
+result = (result << 1) | (b[i] - '0');
+}
 
-        /* Décalage de résultat à gauche et ajout du bit actuel */
-        result = (result << 1) | (b[i] - '0');
-    }
-
-    return (result);
+return (result);
 }
