be69f102895ca3c71023641367255132b1d9dfa8
aurelien <aurelienmest@outlook.fr>
2025-01-09 17:49:13 +0100

    chal8

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -5,7 +5,7 @@
 #include "main.h"
 
 /**
- * read_textfile - Reads a text file and prints it to the POSIX standard output.
+ * read_textfile - Reads a text file and prints it to the POSIX standardtput.
  * @filename: The name of the file to read.
  * @letters: The number of letters it should read and print.
  *
