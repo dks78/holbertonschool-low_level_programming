c5d3b9c25f9d2d18ae2518689096be9695948339
aurelien <aurelienmest@outlook.fr>
2025-01-09 23:22:24 +0100

    chal8

---
+++ malloc_free/main.h
@@ -0,0 +1,18 @@
+#ifndef HOLBERTON_H
+#define HOLBERTON_H
+
+int _putchar(char c);
+int _strlen(char *s);
+char *create_array(unsigned int size, char c);
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
+char *argstostr(int ac, char **av);
+int sum_len(char **av);
+int total_len(int ac, char **av);
+void copy_str(char *a, char *b);
+void copy_strs(char **av, char *s);
+char **strtow(char *str);
+
+#endif /* HOLBERTON_H */
