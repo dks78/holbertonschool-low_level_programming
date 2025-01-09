45181797bba9e66661d8ba9547cb3d341982997d
aurelien <aurelienmest@outlook.fr>
2025-01-09 18:10:51 +0100

    chal8

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -1,12 +1,16 @@
 #include "main.h"
 #include <stdio.h>
- /**
+/**
+ * print_line - Prints a line of underscores based on the input.
+ * @n: The number of underscores to print.
  *
- * print_line - Prints numbers from 0 to 9, excluding 2 and 4.
- * n: charactere
- * Return: Nothing (void)
+ * Description: This function prints a line of underscores ('_') for a given
+ * number of times, which is specified by the input `n`. If `n` is less than
+ * or equal to 0, the function will print nothing. If `n` is positive, the
+ * function will print `n` underscores followed by a new line.
+ *
+ * Return: Nothing (void).
  */
-
 void print_line(int n)
 {
 
