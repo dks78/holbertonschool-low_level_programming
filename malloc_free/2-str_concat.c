7c6e6ed2728f47099f49608aec40113cae91bf4b
aurelien <aurelienmest@outlook.fr>
2025-01-09 23:26:22 +0100

    chal8

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -2,56 +2,55 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * str_concat - concatenates two strings
+ * @s1: first string
+ * @s2: second string
+ * Return: pointer to allocated memory containing concatenated strings or
+ * NULL on failure
+ */
 char *str_concat(char *s1, char *s2)
 {
-    int longeur = 0;
-    int longuer2 = 0;
-    char *result;
-    int i;
-    if(s1 == NULL)
-    {
-        return NULL;
-    }
+	unsigned int i, j, len1, len2;
+	char *str;
 
-    while (s1[longeur ] != '\0') 
-    {
-        longeur ++;
-    }
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
 
+	len1 = _strlen(s1);
+	len2 = _strlen(s2);
 
-    while (s2[longuer2] != '\0') 
-    {
-        longuer2++;
-    }
+	str = malloc(sizeof(char) * (len1 + len2 + 1));
 
-   result = malloc((longeur + longuer2 + 1)); /*
-    La taille allouée est la somme des longueurs de s1 et s2, plus 1 s
-    upplémentaire pour le caractère de fin de chaîne '\0'.
-    allouer (longeur + longuer2 + 1) octets.
-   */
-    if(result == NULL)
-    {
-        return NULL;
-    }
-    
-    for ( i = 0; i < longeur ; i++)
-    {
-        result[i] = s1[i];
-    } /*Cette boucle copie tous les caractères de s1 dans result. Vous commencez à copier à partir de l'index 0 et continuez jusqu'à la fin de s1 (l'index longeur - 1).
-        À la fin de cette boucle, la première partie de result contient la chaîne s1.*/
-    for (i = 0; i < longuer2; i++)
-    {
-        result[longeur + i] = s2[i]; /*
-        Cette boucle copie tous les caractères de 
-        s2 à la suite de s1 dans result. Les caractères de s2 sont copiés à partir de l'index longeur dans result, donc après la fin de la chaîne s1.
-        Pourquoi longeur + i ? Parce que les premiers longeur 
-        indices de result contiennent déjà la chaîne s1. Ensuite, nous ajoutons les caractères de s2 après.
-        */
-    }
+	if (str == NULL)
+		return (NULL);
 
+	for (i = 0; i < len1; i++)
+		str[i] = s1[i];
 
-    result[longeur  + longuer2] = '\0';
+	for (j = 0; j < len2; i++, j++)
+		str[i] = s2[j];
 
+	str[i] = '\0';
 
-    return result;
+	return (str);
+}
+
+/**
+ * _strlen - returns the length of a string
+ * @s: string to be evaluated
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+
+	return (i);
 }
