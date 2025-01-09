e9b9ece244b4042c29fc71163ed9633b2baa959e
aurelien <aurelienmest@outlook.fr>
2025-01-09 18:09:52 +0100

    chal8

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -1,37 +1,39 @@
 #include "main.h"
 #include <stdio.h>
- /**
+/**
+ * more_numbers - Prints numbers from 0 to 14, ten times.
  *
- * more_numbers - Prints numbers from 0 to 9, excluding 2 and 4.
- * n: charactere
- * Return: Nothing (void)
+ * Description: This function prints the numbers from 0 to 14 in a sequence,
+ * and this sequence is repeated 10 times. Numbers are printed in the format
+ * of two digits, e.g., 09 for 9, 10, 11, ..., 14.
+ *
+ * Return: Nothing (void).
  */
 void more_numbers(void)
 {
 
-int i;
-int n;
+	int i;
+	int n;
 
-for(i = 0; i < 10; i++)
+	for (i = 0; i < 10; i++)
+	{
 
-{
+		for (n = 0; n <= 14; n++)
 
-for( n = 0; n <= 14; n++)
+		{
 
-{
+			if (n > 9)
 
-if (n > 9)
+			{
 
-{
+				_putchar('1');
 
-_putchar('1');
+			}
+			_putchar((n % 10) + '0');
 
-}
-_putchar((n % 10) + '0');
+		}
+		_putchar('\n');
 
-}
-_putchar('\n');
-
-}
+	}
 
 }
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -12,4 +12,5 @@ void print_diagonal(int n);
 void print_square(int size);
 void print_string(const char *str);
 void print_triangle(int size);
+void print_most_numbers(void);
 #endif
