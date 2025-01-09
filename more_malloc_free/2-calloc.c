ac5a8bbe3e06c4f56d5a346c6a857b2dfa0511d2
aurelien <aurelienmest@outlook.fr>
2025-01-09 23:39:58 +0100

    chal8

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -1,27 +1,31 @@
 #include "main.h"
-#include <stddef.h>
+#include <stdio.h>
 #include <stdlib.h>
-/**
- * simple_print_buffer - prints buffer in hexa
- * @buffer: the address of memory to print
- * @size: the size of the memory to print
- *
- * Return: Nothing.
- */
 
+/**
+ * _calloc - allocates memory set to zero for an array using malloc
+ * @nmemb: number of elements in array to be allocated for
+ * @size: size in bytes to be allocated for each element
+ * Return: pointer to allocated memory or NULL if fails or nmemb or size is 0
+ */
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-    void *ptr;
-    if(nmemb == 0 || size == 0)
-    {
-        return NULL;
-    }
-    ptr = malloc(nmemb * size);
-    if(ptr == NULL)
-    {
-        return NULL;
-    }
+	unsigned int i;
+	char *array;
+	unsigned int mem;
 
-    return ptr;
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	mem = nmemb * size;
+
+	array = malloc(mem);
+
+	if (array == NULL)
+		return (NULL);
+
+	for (i = 0; i < mem; i++)
+		array[i] = 0;
+
+	return (array);
 }
-
