d9a78191c1a7ea6580cc93ba778aaaa8f76349f6
aurelien <aurelienmest@outlook.fr>
2025-01-09 23:31:17 +0100

    chal8

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -1,35 +1,42 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+/**
+ * alloc_grid - returns a pointer to a two dimensional array of integers
+ * @width: width of array
+ * @height: height of array
+ * Return: pointer to 2D array or NULL on failure
+ */
+int **alloc_grid(int width, int height)
+{
+	int **grid;
+	int i, j;
 
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
 
+	grid = malloc(height * sizeof(int *));
 
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
 
-int **alloc_grid(int width, int height) {  
-    int **grid;
-    int i,j;
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = malloc(width * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(grid[j]);
+			}
+			free(grid);
+			return (NULL);
+		}
+	}
 
-    if (width <= 0 || height <= 0) {
-        return NULL;
-    }
-
-    grid = malloc(height * sizeof(int *));
-
-    if (grid == NULL) {
-        return NULL;
-    }
-
-    for (i = 0; i < height; i++) {
-        grid[i] = malloc(width * sizeof(int));  /*Allocation pour chaque ligne*/ 
-        if (grid[i] == NULL) {
-            /*  Si l'allocation échoue pour une ligne, libérer les lignes précédentes */
-            for ( j = 0; j < i; j++) {
-                free(grid[j]);
-            }
-            free(grid);  /*Libérer la mémoire du tableau de pointeurs*/ 
-            return NULL; 
-        }
-    }
-
-    return grid; 
+	return (grid);
 }
