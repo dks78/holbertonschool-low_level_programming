a8d070e3d4a591b7f27ee2ef8848d8bdf9fdffc1
aurelien <aurelienmest@outlook.fr>
2025-01-09 17:52:27 +0100

    chal8

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -1,11 +1,10 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
-#include <fcntl.h> /* Required for open */
+#include <fcntl.h>
 #include "main.h"
-
 /**
- * read_textfile - Reads a text file and prints it to the POSIX standardtput.
+ * read_textfile - Reads a text file and prints it to the POSIX standard ut.
  * @filename: The name of the file to read.
  * @letters: The number of letters it should read and print.
  *
@@ -24,14 +23,12 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	fd = open(filename, O_RDONLY);
 	if (fd == -1)
 		return (0);
-
 	buffer = malloc(sizeof(char) * letters);
 	if (buffer == NULL)
 	{
 		close(fd);
 		return (0);
 	}
-
 	bytes_read = read(fd, buffer, letters);
 	if (bytes_read == -1)
 	{
@@ -39,7 +36,6 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fd);
 		return (0);
 	}
-
 	bytes_written = write(STDOUT_FILENO, buffer, bytes_read);
 	if (bytes_written == -1 || bytes_written != bytes_read)
 	{
@@ -47,8 +43,8 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		close(fd);
 		return (0);
 	}
-
 	free(buffer);
 	close(fd);
+
 	return (bytes_written);
 }
