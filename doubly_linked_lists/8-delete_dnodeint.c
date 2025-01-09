1c742f170e6b015adeed9457827ad7483a695c95
aurelien <aurelienmest@outlook.fr>
2025-01-09 17:46:18 +0100

    chal8

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -2,47 +2,47 @@
 #include <stdlib.h>
 #include "lists.h"
 /**
- * insert_dnodeint_at_index - Insère un nœud à un index donné dans une liste doublement chaînée
- * @h: Pointeur vers le pointeur de la tête de la liste
- * @idx: Index où insérer le nouveau nœud (commençant à 0)
- * @n: La valeur à insérer dans le nœud
+ * delete_dnodeint_at_index - Supprime un nœud à un index donné dans une
+ *                             doublement chaînée
+ * @head: Pointeur vers le pointeur de la tête de la liste.
+ * @index: Index du nœud à supprimer (commençant à 0).
  *
- * Return: Le pointeur vers le nœud inséré, ou NULL en cas d'échec
+ * Return: 1 si la suppression a réussi, -1 en cas d'échec.
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-    dlistint_t *current = *head;
-    unsigned int i = 0;
+	dlistint_t *current = *head;
+	unsigned int i = 0;
 
-    if (!head || !(*head)) 
-        return (-1);
-
-
-    if (index == 0)
-    {
-        *head = current->next;
-        if (*head)
-            (*head)->prev = NULL;
-        free(current);
-        return (1);
-    }
-
-    while (current && i < index)
-    {
-        current = current->next;
-        i++;
-    }
+	if (!head || !(*head))
+		return (-1);
 
 
-    if (!current)
-        return (-1);
+	if (index == 0)
+	{
+		*head = current->next;
+		if (*head)
+			(*head)->prev = NULL;
+		free(current);
+		return (1);
+	}
 
-    if (current->prev)
-        current->prev->next = current->next;
-    
-    if (current->next)
-        current->next->prev = current->prev;
+	while (current && i < index)
+	{
+		current = current->next;
+		i++;
+	}
 
-    free(current);
-    return (1);
+
+	if (!current)
+		return (-1);
+
+	if (current->prev)
+		current->prev->next = current->next;
+
+	if (current->next)
+		current->next->prev = current->prev;
+
+	free(current);
+	return (1);
 }
