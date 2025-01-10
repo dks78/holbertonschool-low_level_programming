0df39936f409b3400009700f10229524baad261c
aurelien <aurelienmest@outlook.fr>
2025-01-10 09:06:38 +0100

    chal8

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,24 +1,27 @@
 #include "main.h"
 #include <stdio.h>
-
 /**
- * main - check the code
- * @dest - varibale
- * Return: Always 0.
-*/
+ * _strncpy - Copies up to n characters from the string pointed to by src,
+ *            to the array pointed to by dest.
+ * @dest: Pointer to the destination string.
+ * @src: Pointer to the source string.
+ * @n: The maximum number of characters to copy from src.
+ *
+ * Return: A pointer to the destination string dest.
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
-int i;
+	int i;
 
-    for (i = 0; i < n && src[i] != '\0'; i++)
-    {
-        dest[i] = src[i];
-    }
+	for (i = 0; i < n && src[i] != '\0'; i++)
+	{
+		dest[i] = src[i];
+	}
 
-    for (; i < n; i++)
-    {
-        dest[i] = '\0';
-    }
+	for (; i < n; i++)
+	{
+		dest[i] = '\0';
+	}
 
-    return (dest);
+	return (dest);
 }
